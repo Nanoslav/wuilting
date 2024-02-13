f00c1cc40ef297c6c744de716a416b22f8b63965
František Kříž <thenanoslav@gmail.com>
2024-02-13 10:33:53 +0100

    Update Navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,46 +1,106 @@
+import Link from "next/link";
+
 export const Navbar = () => {
-    return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                  d="M4 6h16M4 12h16M4 18h7"/>
-                        </svg>
-                    </div>
-                    <ul tabIndex={0}
-                        className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a href='/'>Home</a></li>
-                        <li><a href='/login'>Login</a></li>
-                        <li><a href='/shop'>Shop</a></li>
-                        <li><a href='/about'>About us</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div className="navbar-center">
-                <a className="btn btn-ghost text-xl">💫 Wuilting 💫</a>
-            </div>
-            <div className="navbar-end">
-                <button className="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
-                    </svg>
-                </button>
-                <button className="btn btn-ghost btn-circle">
-                    <div className="indicator">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
-                        </svg>
-                        <span className="badge badge-xs badge-primary indicator-item"></span>
-                    </div>
-                </button>
-            </div>
+  // return (
+  //     <div className="navbar bg-base-100">
+  //         <div className="navbar-start">
+  //             <div className="dropdown">
+  //                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+  //                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+  //                          stroke="currentColor">
+  //                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+  //                               d="M4 6h16M4 12h16M4 18h7"/>
+  //                     </svg>
+  //                 </div>
+  //                 <ul tabIndex={0}
+  //                     className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+  //                     <li><a href='/'>Home</a></li>
+  //                     <li><a href='/login'>Login</a></li>
+  //                     <li><a href='/shop'>Shop</a></li>
+  //                     <li><a href='/about'>About us</a></li>
+  //                 </ul>
+  //             </div>
+  //         </div>
+  //         <div className="navbar-center">
+  //             <a className="btn btn-ghost text-xl">💫 Wuilting 💫</a>
+  //         </div>
+  //         <div className="navbar-end">
+  //             <button className="btn btn-ghost btn-circle">
+  //                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+  //                      stroke="currentColor">
+  //                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+  //                           d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
+  //                 </svg>
+  //             </button>
+  //             <button className="btn btn-ghost btn-circle">
+  //                 <div className="indicator">
+  //                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+  //                          stroke="currentColor">
+  //                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+  //                               d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
+  //                     </svg>
+  //                     <span className="badge badge-xs badge-primary indicator-item"></span>
+  //                 </div>
+  //             </button>
+  //         </div>
+  //     </div>
+  // );
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link href="/about">About us</Link>
+            </li>
+          </ul>
         </div>
-    );
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/shop">Shop</Link>
+          </li>
+          <li>
+            <Link href="/about">About us</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <Link href="/login" className="btn">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
 };
