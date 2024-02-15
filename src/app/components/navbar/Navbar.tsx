f630382cd5a909a036480c93ca6c10bfd38a22eb
NinjonikSVK <34453649+Ninjonik@users.noreply.github.com>
2024-02-15 18:15:55 +0100

    - fixed stuff

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export const Navbar = () => {
 
     return (
-        <div className="navbar bg-base-100 px-0.5/10 z-10">
+        <div className="navbar bg-base-100 px-0.5/10 z-10 opacity-100">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
