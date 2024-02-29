739d914696e1080932093ddf49bb4a95f1f8d750
Peter <petricekzatko@gmail.com>
2024-02-29 21:33:20 +0100

    - bug fix

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -6,7 +6,6 @@ const Page = () => (
         <div className="w-3/4 p-6 m-auto rounded-md shadow-md bg-base-100 overflow-y-auto h-2/3">
             <h1 className="mb-5 font-semibold text-center text-purple-700 text-gray-300">Privacy Policy</h1>
             <div>
-                <h1>Privacy Policy</h1>
                 <p>Last updated: February 27, 2024</p>
                 <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
                 <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the <a
